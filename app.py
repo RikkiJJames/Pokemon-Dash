8372a4a814f8f9be7ca739d41a1ced7c219e8177
Rikki James <73121748+RikkiJJames@users.noreply.github.com>
2022-08-20 14:53:39 +0100

    Update app.py
    Added Line graph

--- app.py
+++ app.py
@@ -14,10 +14,18 @@ from dash.dependencies import Output, Input
 #pokemon information
 url = "https://gist.githubusercontent.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6/raw/92200bc0a673d5ce2110aaad4544ed6c4010f687/pokemon.csv"
 data = pd.read_csv(url, index_col = 0)
+data.loc[data['Name'].str.contains('Mega'), 'sport'] = 'ball sport'
+print(data.columns)
+#Removing other characters from Mega Pokemon
+#data['OtherName'] = data['Name'].str.extract(r'(^.*Mega.*$)', expand=False)
+#print("printing" + data["OtherName"])
 
-searchable_columns = data.columns[3:].tolist()
+#Selecting Columns to be searched in radar graph
+searchable_columns = ["Attack","Defense","HP","Speed", "Sp. Atk","Sp. Def"]
 searchable_columns.sort()
-print(searchable_columns)
+
+
+
 #stylesheets
 external_stylesheets = [
     {
@@ -27,9 +35,10 @@ external_stylesheets = [
     },
 ]
 
-print(data.head())
+#print(data.head())
 
 app = dash.Dash(__name__)
+app.title = "Poké App"
 
 app.layout = html.Div(
     children=[
@@ -55,7 +64,7 @@ app.layout = html.Div(
                                 {"label": stats, "value": stats}
                                 for stats in searchable_columns
                             ],
-                            value = ["Attack"],
+                            value = searchable_columns,
                             searchable = True,
                             clearable = False,
                             multi = True,
@@ -74,15 +83,26 @@ app.layout = html.Div(
                                 for name in data["Name"]
                             ],
                             #value = data["Name"],
-                            value = ["Charmander"],
+                            value = ["Bulbasaur"],
                             searchable = True,
                             clearable = True,
                             multi = True,
                             placeholder="Select a pokemon",
                             className="dropdown",
-                        )
+                        ),    
                 ],
-                className = "menu"
+
+            ),
+            html.Div(
+                children =[
+                    html.Div(children="Graph Type", className="menu-title"),
+                        dcc.RadioItems(
+                            id="graph-filter",
+                            options=["Radar", "Line"],
+                            value = "Radar",
+                        ),
+                ],
+
             ),
             html.Div(
                 dcc.Graph(
@@ -91,6 +111,7 @@ app.layout = html.Div(
                 className = "card",
             ) 
         ],
+        className = "menu",
     )
    #className = "wrapper",
    ]
@@ -100,44 +121,66 @@ app.layout = html.Div(
     Output("poke-chart", 'figure'),
     [
      Input("stat-filter", "value"),
-     Input("pokemon-filter", "value")
+     Input("pokemon-filter", "value"),
+     Input("graph-filter", "value")
     ],
 )
-def update_charts(statistics, name):
-
-    if name == None:
+def update_charts(statistics, name, graph_type):
+    
+    #print("name before is")
+    #print(name)
+    
+    if name == []:
         name = data["Name"]
     else:
         pass
     
-    
+    count = 0
+    #print("name after is ")
+    #print(name)
     attributes = statistics
     
     fig = go.Figure()
-
-    for pokemon in name:
-        print(pokemon)
-        print(type(data[statistics] [data["Name"] == pokemon].values.tolist()))
-        print(data[statistics] [data["Name"] == pokemon].values.tolist()[0])
-        fig.add_trace(
-            go.Scatterpolar(
-                r = data[statistics] [data["Name"] == pokemon].values.tolist()[0],
-                theta = attributes,
-                name = pokemon,
-                fill = "toself",
+    
+    if graph_type == "Radar":
+        for pokemon in name:
+            #print(pokemon)
+            #print(type(data[statistics] [data["Name"] == pokemon].values.tolist()))
+            #print(data[statistics] [data["Name"] == pokemon].values.tolist()[0])
+            
+            fig.add_trace(
+                go.Scatterpolar(
+                    r = data[statistics] [data["Name"] == pokemon].values.tolist()[0],
+                    theta = attributes,
+                    name = f"#{data[data['Name'] == pokemon].index[0]}: {pokemon}",
+                    fill = "toself",
+                )
             )
-        )
-
+        fig.update_layout(
+            title_text = "Pokemon Attributes",
+            polar=dict(
+                radialaxis=dict(
+                visible=True,
+                range = [0, data[statistics][data["Name"] == pokemon].max()]
+                )),
+            showlegend=True
+            )
+    else:
+        for pokemon in name:
+            y_values = data[statistics][data["Name"] == pokemon].values.tolist()[0]
+            print(y_values)
+            fig.add_trace(go.Scatter(x= [statistics] * len(y_values), y = y_values,
+                    mode='lines+markers',
+                    name = f"#{data[data['Name'] == pokemon].index[0]}: {pokemon}"
+                    )
+            )
+            
+       
+            
+        fig.update_layout(showlegend = True)
+  
         
-    fig.update_layout(
-        title_text = "Pokemon Attributes",
-        polar=dict(
-            radialaxis=dict(
-            visible=True,
-            range = [0,150]
-            )),
-        showlegend=True
-        )
+    
     
     #print(type(data[statistics] [data["Name"] == name]))
     #print(statistics)
